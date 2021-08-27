bc5c28326d601d8e51f97d3ce967f3db40bfe65a
Gvynblade <wow-trp@yandex.ru>
2021-08-27 16:08:27 +0300

    few fixes in api.js

--- src/api/api.js
+++ src/api/api.js
@@ -4,8 +4,8 @@ export const IngredientsAPI = {
 
     async requestIngredients (setter) {
         try {
-            let response = await fetch (APIUrlIngredients, {withCredentials: true})
-            let data = await response.json()
+            const response = await fetch (APIUrlIngredients, {withCredentials: true})
+            const data = await response.json()
             setter(data.data)
         } catch (error) {
             console.log(error);
